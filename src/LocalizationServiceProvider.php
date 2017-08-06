a8f782cde41c422b8c5bb62a1e8efb38180d15a4
Josias Montag <josias@montag.info>
2017-08-06 15:41:14 +0200

    Fix artisan route:cache

--- src/LocalizationServiceProvider.php
+++ src/LocalizationServiceProvider.php
@@ -42,8 +42,30 @@ class LocalizationServiceProvider extends ServiceProvider
 
 
         $this->app->singleton('url', function($app) {
+
+            // see: Illuminate\Routing\RoutingServiceProvider
+
             $routes = $app['router']->getRoutes();
-            return new LocalizedUrlGenerator($routes, $app->make('request'));
+
+            // The URL generator needs the route collection that exists on the router.
+            // Keep in mind this is an object, so we're passing by references here
+            // and all the registered routes will be available to the generator.
+            $app->instance('routes', $routes);
+
+            $url = new LocalizedUrlGenerator($routes, $app->make('request'));
+
+            $url->setSessionResolver(function () {
+                return $this->app['session'];
+            });
+
+            // If the route collection is "rebound", for example, when the routes stay
+            // cached for the application, we will need to rebind the routes on the
+            // URL generator instance so it has the latest version of the routes.
+            $app->rebinding('routes', function ($app, $routes) {
+                $app['url']->setRoutes($routes);
+            });
+
+            return $url;
         });
 
 
