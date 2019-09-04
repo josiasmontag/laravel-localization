586322962019d1443abc133fc035b67e6b58a4ab
Josias Montag <josias@montag.info>
2019-09-04 11:43:16 +0200

    Simplify UrlGenerator overwriting
    by https://github.com/laravel/framework/pull/28687

--- src/LocalizationServiceProvider.php
+++ src/LocalizationServiceProvider.php
@@ -43,9 +43,6 @@ class LocalizationServiceProvider extends ServiceProvider
 
 
         $this->app->singleton('url', function ($app) {
-
-            // see: Illuminate\Routing\RoutingServiceProvider
-
             $routes = $app['router']->getRoutes();
 
             // The URL generator needs the route collection that exists on the router.
@@ -53,27 +50,11 @@ class LocalizationServiceProvider extends ServiceProvider
             // and all the registered routes will be available to the generator.
             $app->instance('routes', $routes);
 
-            $url = new LocalizedUrlGenerator($routes, $app->make('request'), $app['config']['app.asset_url']);
-
-            // Next we will set a few service resolvers on the URL generator so it can
-            // get the information it needs to function. This just provides some of
-            // the convenience features to this URL generator like "signed" URLs.
-            $url->setSessionResolver(function () {
-                return $this->app['session'];
-            });
-
-            $url->setKeyResolver(function () {
-                return $this->app->make('config')->get('app.key');
-            });
-
-            // If the route collection is "rebound", for example, when the routes stay
-            // cached for the application, we will need to rebind the routes on the
-            // URL generator instance so it has the latest version of the routes.
-            $app->rebinding('routes', function ($app, $routes) {
-                $app['url']->setRoutes($routes);
-            });
-
-            return $url;
+            return new LocalizedUrlGenerator(
+                $routes, $app->rebinding(
+                'request', $this->requestRebinder()
+            ), $app['config']['app.asset_url']
+            );
         });
 
 
